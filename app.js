70cfb91d4c14eae420a67136ae861feac297a3f5
Miku Suga <mikusuga@gmail.com>
2020-11-05 07:26:51 +0700

    done

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var corsApiUrl = "https://cors-anywhere.herokuapp.com/";
 // TODO: REPLACE YOUR TOKEN
-var apiToken = "?token=YOUR_TOKEN_HERE";
+var apiToken = "?token=sW0VHwiJxiwpcc3aylLxILqg4tCsIpW87Obn6RGGQ0s";
 
 // CORS stands for "cross origin resource sharing" -- you'll be making http requests in order
 // DON'T CHANGE THIS: fetches the data from the API endpoint
@@ -25,7 +25,33 @@ corsPromise().then(
   (request) =>
     (request.onload = request.onerror = function () {
       // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
+      // showPlant(request.response)
+      var plantList = JSON.parse(request.response);
+      plantData = plantList.data;
+
+      const plantsFiltered = plantData.filter((arrayItem) => {
+        return arrayItem.family_common_name=="Rose family";
+      })
+
+      plantsFiltered.map((arrayItem) => {
+        const wrapper = document.createElement("div");
+        const name = document.createElement("h4");
+        const img = document.createElement("img");
+        wrapper.setAttribute("margin", "5%");
+        name.innerText = arrayItem.common_name;
+        img.setAttribute("src", arrayItem.image_url);
+        img.setAttribute("width", 250);
+        wrapper.appendChild(name)
+        wrapper.appendChild(img);
+        document.getElementById("plants").appendChild(wrapper);
+      });
+
+
     })
 );
 
 //// TODO: ADD WHATEVER FUN CONTENT YOU WANT ////
+
+
+
+  
